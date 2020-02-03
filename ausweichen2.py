7262794aa816c1f9ce5701c52526fe4be069904c
herrmann-michi <60224027+herrmann-michi@users.noreply.github.com>
2020-02-03 12:30:57 +0100

    Add files via upload

--- ausweichen2.py
+++ ausweichen2.py
@@ -2,60 +2,53 @@ def main():
     import sys
     import time
     import ctypes
+    import pigpio
     ##import cv2 as cv
 
     from botlib.bot import Bot
     from readchar import readkey, key
     from ultra import Sonic
 
-    sonic=Sonic()
     bot = Bot()
     STEP_POWER, STEP_STEER= 10, 0.25
     power, steer = 0, 0.0
     running = True
+    checkdistance = False
     objectsafe = 0
 
     print('calibrating...')
     bot.calibrate()
 
     while running:
-        ##objects = bot.detectObject("../classifer/cascade.xml")
         inp = readkey()
-        distance = sonic.getDistance()
-
-        ##ret, frame = bot.getCap().read()
-        if distance[1] < 30 or distance[2] < 30: 
-            print('object detected...')
-            bot.drive_power(0)
-            time.sleep(0.5)
-            bot.drive_steer(1.)
-            bot.drive_power(60)
-            time.sleep(0.4)
-            bot.drive_power(0)
-            bot.drive_steer(-1.0)
-            bot.drive_power(60)
-            time.sleep(1.4)
-            bot.drive_power(0)
-            bot.drive_steer(1.0)
-            bot.drive_power(60)
-            time.sleep(0.4)
-            bot.drive_steer(0)
-            time.sleep(1)
-            bot.drive_power(0)
-        else:
-            bot.drive_power(0)
-            print('no other bot detected...')
-            print('waiting for free path...')
-            if distance[1] > 50  or distance[2] > 50:
-                bot.drive_power(80)
-            else:
-                print('move object out of the way...')
-        
-        if inp == 'o':
-            if objectsafe == 0:
-                objectsafe = 1
-            else:
-                objectsafe = 0
+        if inp = 's':
+            checkdistance = True        
+            while checkdistance:
+                if distance[0] < 30 or distance[1] < 30: 
+                    print('object detected...')
+                    bot.drive_power(0)
+                    time.sleep(0.5)
+                    bot.drive_steer(1.)
+                    bot.drive_power(60)
+                    time.sleep(0.4)
+                    bot.drive_power(0)
+                    bot.drive_steer(-1.0)
+                    bot.drive_power(60)
+                    time.sleep(1.4)
+                    bot.drive_power(0)
+                    bot.drive_steer(1.0)
+                    bot.drive_power(60)
+                    time.sleep(0.4)
+                    bot.drive_steer(0)
+                    time.sleep(1)
+                    bot.drive_power(0)
+                else:
+                    bot.drive_power(0)
+                    while distance[0] < 50 or distance[1] 50:
+                        print('move object out of the way')
+                    bot.drive_power(50)
+                if key = 't':
+                    checkdistance=False
                     
         if inp == key.SPACE:
             print('stop...')
