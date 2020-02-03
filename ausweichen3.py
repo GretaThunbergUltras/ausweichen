e269c4c1f7490f28c2731173a67a119957a3604c
CassianGallon <60434689+CassianGallon@users.noreply.github.com>
2020-02-03 16:18:01 +0000

    Create ausweichen3.py

---
+++ ausweichen3.py
@@ -0,0 +1,66 @@
+def main():
+    import sys
+    import time
+    import ctypes
+    ##import cv2 as cv
+
+    from botlib.bot import Bot
+    from readchar import readkey, key
+    from ultra import Sonic
+
+    sonic=Sonic()
+    bot = Bot()
+    STEP_POWER, STEP_STEER= 10, 0.25
+    power, steer = 0, 0.0
+    running = True
+    objectsafe = 0
+
+    print('calibrating...')
+    bot.calibrate()
+
+    while running:
+        ##objects = bot.detectObject("../classifer/cascade.xml")
+        inp = readkey()
+        distance = sonic.getDistance()
+
+        ##ret, frame = bot.getCap().read()
+        if distance[1] < 30 or distance[2] < 30: 
+            print('object detected...')
+            bot.drive_power(0)
+            time.sleep(0.5)
+            bot.drive_steer(1.)
+            bot.drive_power(60)
+            time.sleep(0.4)
+            bot.drive_power(0)
+            bot.drive_steer(-1.0)
+            bot.drive_power(60)
+            time.sleep(1.4)
+            bot.drive_power(0)
+            bot.drive_steer(1.0)
+            bot.drive_power(60)
+            time.sleep(0.4)
+            bot.drive_steer(0)
+            time.sleep(1)
+            bot.drive_power(0)
+        else:
+            bot.drive_power(0)
+            print('no other bot detected...')
+            print('waiting for free path...')
+            if distance[1] > 50  or distance[2] > 50:
+                bot.drive_power(80)
+            else:
+                print('move object out of the way...')
+        
+        if inp == 'o':
+            if objectsafe == 0:
+                objectsafe = 1
+            else:
+                objectsafe = 0
+                    
+        if inp == key.SPACE:
+            print('stop...')
+            bot.stop_all()
+            running = False
+
+if __name__ == '__main__':
+    main()
