8a3255495f345d9bc57cc05c79d81792ad821bac
herrmann-michi <60224027+herrmann-michi@users.noreply.github.com>
2020-02-04 22:12:30 +0100

    Update ausweichen2.py

--- ausweichen2.py
+++ ausweichen2.py
@@ -1,29 +1,36 @@
 def main():
+    ##import Library
     import sys
     import time
     import ctypes
     import pigpio
 
+    ##create new instance for bot, readchar and ultra
     from botlib.bot import Bot
     from readchar import readkey, key
     from ultra import Sonic
 
     bot = Bot()
     sonic = Sonic()
+    
+    ##set parameter for while loops
     running = True
     checkdistance = False
-    objectsafe = 0
     
+    ##calibrate bot
     print('calibrating...')
     bot.calibrate()
-
+    
+    ##while loop for main programm
     while running:
         inp = readkey()
         if inp == 's':
             checkdistance = True        
             while checkdistance:
                 distance = sonic.getDistance()
+                ##if any distance < 50 start avoid sequence 
                 if distance[0] < 50 or distance[1] < 50:
+                    #avoid to the rigth side
                     bot.drive_power(0)
                     time.sleep(0.5)
                     bot.drive_steer(1.)
@@ -40,9 +47,11 @@ def main():
                     bot.drive_steer(0)
                     time.sleep(1)
                     bot.drive_power(0)
+                ##if not obstacle in the way drive forward
                 else:
                     bot.drive_power(30)
-        inp = readkey()            
+        inp = readkey()
+        ##if Space is spressed stop the while loop and stop all movement
         if inp == key.SPACE:
             print('stop...')
             bot.stop_all()
